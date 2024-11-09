4e364165ee4ec40c3d905ed6152a6440ce782eaa
idkos <leegang31@gmail.com>
2024-11-10 03:40:58 +0900

    chore: Signup Button id 변경

--- app/src/main/java/com/example/loginapp/ui/LoginFragment.kt
+++ app/src/main/java/com/example/loginapp/ui/LoginFragment.kt
@@ -25,7 +25,7 @@ class LoginFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.signupButton.setOnClickListener {
+        binding.btnNavigateSignup.setOnClickListener {
             (activity as? MainActivity)?.switchFragment(SignupFragment())
         }
     }
