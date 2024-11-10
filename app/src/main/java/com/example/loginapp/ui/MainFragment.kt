07b2b17f5720f3fcb6c488b99389bb304460a096
idkos <leegang31@gmail.com>
2024-11-10 21:20:52 +0900

    feat: main 프래그먼트 생성

---
+++ app/src/main/java/com/example/loginapp/ui/MainFragment.kt
@@ -0,0 +1,40 @@
+package com.example.loginapp.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.loginapp.databinding.FragmentMainBinding
+import com.example.loginapp.model.User
+
+class MainFragment : Fragment() {
+    private var _binding: FragmentMainBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentMainBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val user: User? = arguments?.getParcelable("user")
+
+        binding.run {
+            tvName.text = user?.name
+            tvNickname.text = user?.nickname
+            tvEmail.text = user?.email
+            tvPassword.text = user?.password
+        }
+    }
+
+}
