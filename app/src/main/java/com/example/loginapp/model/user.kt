0894937088753600dbae5e24e5cf774fa4fb6df5
idkos <leegang31@gmail.com>
2024-11-10 21:27:05 +0900

    chore: Parcelize 추가

--- app/src/main/java/com/example/loginapp/model/user.kt
+++ app/src/main/java/com/example/loginapp/model/user.kt
@@ -1,8 +1,12 @@
 package com.example.loginapp.model
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class User(
     val name: String,
     val nickname: String,
     val email: String,
     val password: String
-)
+) : Parcelable
